db0575fc2c543f1d9aa21585802f30bf47bf564f
Diego Romero-Cardona <dr0887@uah.edu>
2026-09-07 22:18:45 -0500

    Stop comms on ctrl c

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data import Data
 from communication import Communication
+import signal
 
 class WebGroundStation:
     def __init__(self):
@@ -9,6 +10,8 @@ class WebGroundStation:
         self.data.load() # Load configuration data
 
         self.communication = Communication(self.data.config['port'], self.data.config['baudrate'], self.data.config['csv'])
+        
+        signal.signal(signal.SIGINT, self.communication.stop)
 
 if __name__ == "__main__":
     gs = WebGroundStation()
